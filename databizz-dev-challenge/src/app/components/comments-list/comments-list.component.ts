8f158e068c50f4785f6947bba6c8a1d2b05b78ef
eulaliaaires <memgra@cin.ufpe.br>
2020-08-09 23:04:38 -0300

    feature: add read data function

--- databizz-dev-challenge/src/app/components/comments-list/comments-list.component.ts
+++ databizz-dev-challenge/src/app/components/comments-list/comments-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentsService } from '../../shared/comments.service';
 
 @Component({
   selector: 'app-comments-list',
   templateUrl: './comments-list.component.html',
-  styleUrls: ['./comments-list.component.css']
+  styleUrls: ['./comments-list.component.css'],
 })
 export class CommentsListComponent implements OnInit {
+  comments: any;
+  caja: any;
+  constructor(public commentsService: CommentsService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.commentsService.read_Comments().subscribe((data) => {
+      this.comments = data.map((e) => {
+        return {
+          id: e.payload.doc.id,
+          Author: e.payload.doc.data()['Author'],
+          Message: e.payload.doc.data()['Message'],
+        };
+      });
+      console.log('comments', this.comments);
+    });
   }
-
 }
