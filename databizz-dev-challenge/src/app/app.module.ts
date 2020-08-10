513629fb4a1bd6fc11676416e4c0f5e1a62b25e3
eulaliaaires <memgra@cin.ufpe.br>
2020-08-09 22:28:19 -0300

    feature: create comments list component

--- databizz-dev-challenge/src/app/app.module.ts
+++ databizz-dev-challenge/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CommentsComponent } from './components/comments/comments.component';
+import { CommentsListComponent } from './components/comments-list/comments-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentsComponent
+    CommentsComponent,
+    CommentsListComponent
   ],
   imports: [
     BrowserModule
